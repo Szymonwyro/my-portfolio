0d6f44d7ed513cf4d65681569b8f6e199932f5d5
Szymon Wyrozumski <szymwyro@icloud.com>
2025-12-09 14:04:39 +0000

    add preferred stack with images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6" style={{ maxWidth: '948px', width: '100%' }}>
 
         <div className="flex-1 bg-black border border-gray-900 rounded-2xl p-6">
-          <h2 className="text-2xl font-semi-bold mb-4 text-white">Education</h2>
+          <h2 className="text-2xl font-bold mb-4 text-white">Education</h2>
           <ul className="text-white list-disc list-outside pl-4 space-y-2">
             <li>MEng Engineering Mathematics, Bristol University </li>
             <li>A-Levels: <strong>A*AAA + A*</strong><br />
@@ -46,7 +46,7 @@ export default function Home() {
       
 
         <div className="flex-1 bg-black border border-gray-900 rounded-2xl p-6">
-          <h2 className="text-2xl font-semi-bold mb-4 text-white">Contact</h2>
+          <h2 className="text-2xl font-bold mb-4 text-white">Contact</h2>
           <ul className="text-white list-disc list-outside pl-4 space-y-2">
             <li>Email: sz.wyrozumski@gmail.com </li>
             <li>LinkedIn: Szymon Wyrozumski</li>
@@ -58,12 +58,24 @@ export default function Home() {
       <div className="mt-6 grid grid-cols-1 md:grid-cols-1 gap-6" style={{ maxWidth: '948px', width: '100%' }}>
 
         <div className="flex-1 bg-black border border-gray-900 rounded-2xl p-6">
-          <h2 className="text-2xl font-semi-bold mb-4 text-white">Projects</h2>
+          <h2 className="text-2xl font-bold mb-4 text-white">Projects</h2>
           <ul className="text-white list-disc list-outside pl-4 space-y-2">
             <li><strong>Extended Project Qualification (EPQ)</strong> - A detailed analysis of relevant literature on HFT's market impact, with regards to efficiency, strategy and regulation. <strong>Achieved 94% (A*)</strong></li>
             <li><strong>Machine Learning strategy comparison for FX</strong> - A comparison between a macroeconomic-integrated ML model vs a price-action only ML model to optimise predictive power in FX markets (EUR/GBP/USD/JPY)</li>
             <li><strong>C++ Backtesting Engine</strong> - A C++ based engine for backtesting trading strategies with a focus on performance and accuracy.</li>
           </ul>
+        </div>
+      </div>
+
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-1 gap-6" style={{ maxWidth: '948px', width: '100%' }}>
+        <h1 className="text-center text-white text-5xl font-bold mb-4"> Preferred Tech</h1>
+        <div className="mt-4 flex justify-center space-x-8">
+          <img src="/photos/python.jpg" alt="Python" className="h-16 w-16 object-contain" />
+          <img src="/photos/cpp.jpg" alt="C++" className="h-16 w-16 object-contain" />
+          <img src="/photos/react.jpg" alt="React" className="h-16 w-16 object-contain" />
+          <img src="/photos/typescript.jpg" alt="TypeScript" className="h-16 w-16 object-contain" />
+          <img src="/photos/nextjs.jpg" alt="Next.js" className="h-16 w-16 object-contain" />
+          <img src="/photos/git.jpg" alt="Git" className="h-16 w-16 object-contain" />
         </div>
       </div>
     </main>
